50295c7fb8628f6d45d931c386c5e29d73e8af42
Lara Poves Martinez <lara@landau.etsit.urjc.es>
2024-03-05 18:19:58 +0100

    teleoperador

--- src/teleoperator.py
+++ src/teleoperator.py
@@ -1,59 +1,62 @@
 import carla
-import math
 import pygame
 import numpy as np
 import time
 
-# Spectator
-SCALE = 5.0
-ELEVATION = 3.0
-PITCH = -10.0
-
 # Screen
-HEIGHT_SCREEN = 500
-WIDTH_SCREEN = 700
+HEIGHT_SCREEN = 600
+WIDTH_SCREEN = 600
+ELEVATION = 2.5
 
 # Control velocity 
 BRAKE = 1.0
 STEER = 0.4
 THROTTLE = 0.7
-INCREMENT = 0.1
 
-# Global variables
-camera = None
-image_stream = None
+class Camera_screen:
+    def __init__(self, name, vehicle, world, num_screen, transform=carla.Transform()):
+        pygame.display.set_caption(name)
+        self.screen = pygame.Surface((WIDTH_SCREEN, HEIGHT_SCREEN))
+        self.screen_rect = self.screen.get_rect(topleft=((num_screen - 1) * WIDTH_SCREEN, 0))
+        self.image = None
 
-def center_spectator(spectator, transform):
-    spectator_transform = carla.Transform()
-    yaw = math.radians(transform.rotation.yaw)
+        # Add camera
+        camera_bp = world.get_blueprint_library().find('sensor.camera.rgb')
+        self.camera = world.spawn_actor(camera_bp, transform, attach_to=vehicle)
+        self.camera.listen(lambda image: self.update_image(image))
 
-    spectator_transform.location.z = transform.location.z + ELEVATION
-    spectator_transform.location.x = transform.location.x - SCALE * math.cos(yaw)
-    spectator_transform.location.y = transform.location.y - SCALE * math.sin(yaw)
-    spectator_transform.rotation.pitch = PITCH
-    spectator_transform.rotation.yaw = transform.rotation.yaw 
+    def update_image(self, image):
+        self.image = image
 
-    spectator.set_transform(spectator_transform)
+    def show_camera(self, screen):
+        if self.image is not None:
+            # Convert the image to a numpy array
+            array = np.frombuffer(self.image.raw_data, dtype=np.dtype("uint8"))
+            array = np.reshape(array, (self.image.height, self.image.width, 4))
 
-def setup_carla(port=2000, name_world='Town01', vehicle='vehicle.lincoln.mkz_2020', transform=carla.Transform()):
+            # Convert RGBA to RGB
+            array = array[:, :, :3]
+
+            # Create a Pygame surface 
+            image_surface = pygame.surfarray.make_surface(array)
+
+            # Resize the image
+            screen_surface = pygame.transform.scale(image_surface, (WIDTH_SCREEN, HEIGHT_SCREEN))
+            
+            screen.blit(screen_surface, self.screen_rect)
+
+def setup_carla(port=2000, vehicle='vehicle.lincoln.mkz_2020', name_world='Town01', transform=carla.Transform()):
+    # Connect to the server
     client = carla.Client('localhost', port)
     world = client.get_world()
     client.load_world(name_world)
 
-    # Retrieve and locate spectator object
-    spectator = world.get_spectator()
-    transform.rotation.pitch = 0.0
-    transform.rotation.roll = 0.0
-    transform.location.z = 0.0
-    center_spectator(spectator, transform)
-    transform.location.z = ELEVATION
-
-    # Create Ego Vehicle and spawn it at spectator's location
+    # Create and locate ego vehicle 
     ego_bp = world.get_blueprint_library().find(vehicle)
     ego_bp.set_attribute('role_name', 'hero')
     ego_vehicle = world.spawn_actor(ego_bp, transform)
 
-    # Configure Ego Vehicle
+    # Configure ego vehicle's control
     control = carla.VehicleControl()
     control.throttle = 0.0  
     control.steer = 0.0     
@@ -62,53 +65,17 @@ def setup_carla(port=2000, name_world='Town01', vehicle='vehicle.lincoln.mkz_202
     control.reverse = False  
     control.manual_gear_shift = False
     ego_vehicle.apply_control(control)
-    return world, spectator, ego_vehicle
 
-def add_camera(vehicle, world):
-    global camera
+    return world, ego_vehicle
 
-    # Create a transform to place the camera on top of the vehicle
-    camera_init_trans = carla.Transform(carla.Location(z=2.5, x=0.5), carla.Rotation(pitch=-10.0, roll=90.0))
-
-    # We create the camera through a blueprint that defines its properties
-    camera_bp = world.get_blueprint_library().find('sensor.camera.rgb')
-
-    # We spawn the camera and attach it to our ego vehicle
-    camera = world.spawn_actor(camera_bp, camera_init_trans, attach_to=vehicle)
-
-    return camera
-
-def setup_pygame():
+def setup_pygame(num_screen):
     pygame.init()
-    pygame.display.set_caption('Camera stream')
-    screen = pygame.display.set_mode((WIDTH_SCREEN, HEIGHT_SCREEN), 0)
     clock = pygame.time.Clock()
 
+    screen = pygame.display.set_mode((WIDTH_SCREEN * num_screen, HEIGHT_SCREEN))
+    pygame.display.set_caption('Stream cameras')
+
     return screen, clock
-
-def update_image(image):
-    global image_stream
-    image_stream = image
-
-def show_camera(screen):
-    global image_stream
-
-    if image_stream is not None:
-        # Convert the image to a numpy array
-        array = np.frombuffer(image_stream.raw_data, dtype=np.dtype("uint8"))
-        array = np.reshape(array, (image_stream.height, image_stream.width, 4))
-
-        # Convert RGBA to RGB
-        array = array[:, :, :3]
-
-        # Create a Pygame surface 
-        image_surface = pygame.surfarray.make_surface(array)
-
-        # Resize the image
-        screen_surface = pygame.transform.scale(image_surface, (WIDTH_SCREEN, HEIGHT_SCREEN))
-        
-        screen.blit(screen_surface, (0, 0))
-        pygame.display.flip()
 
 def update_vel(vehicle):
     control = vehicle.get_control()
@@ -129,22 +96,25 @@ def update_vel(vehicle):
     vehicle.apply_control(control)
     
 def main():
-    global camera
-
     # Setup CARLA and pygame
-    world, spectator, ego_vehicle = setup_carla(name_world='Town03', transform=carla.Transform(carla.Location(x=100.0, y=-6.0)))
-    screen, clock = setup_pygame()
+    vehicle_transform = carla.Transform(carla.Location(x=100.0, y=-6.0, z=ELEVATION))
+    world, ego_vehicle = setup_carla(name_world='Town03', transform=vehicle_transform)
+    screen, clock = setup_pygame(num_screen=2)
 
-    print("Preparing Carla and Pygame...")
-    time.sleep(3)
+    print("Preparing Carla and pygame...")
+    time.sleep(2)
     print("Setup completed")
 
-    # Add camera
-    camera = add_camera(vehicle=ego_vehicle, world=world)
-    camera.listen(lambda image: update_image(image))
+    # Create cameras' screens
+    driver_transform = carla.Transform(carla.Location(z=ELEVATION, x=0.5), carla.Rotation(pitch=-10.0, roll=90.0))
+    driver = Camera_screen(name='Stream camera driver', num_screen=1, vehicle=ego_vehicle, world=world, transform=driver_transform)
+
+    spectator_transform = carla.Transform(carla.Location(x=-4.0, z=ELEVATION), carla.Rotation(pitch=-10.0, roll=90.0))
+    spectator = Camera_screen(name='Teleoperator', num_screen=2, vehicle=ego_vehicle, world=world, transform=spectator_transform)
 
     try:
         while True:
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -152,9 +122,10 @@ def main():
                 elif event.type == pygame.KEYDOWN:
                     update_vel(ego_vehicle)
 
-            center_spectator(spectator, ego_vehicle.get_transform())
-            show_camera(screen)
-            clock.tick(600) # Frame rate
+            driver.show_camera(screen)
+            spectator.show_camera(screen)
+            pygame.display.flip()
+            clock.tick(60) # Frame rate
 
     except KeyboardInterrupt:
         pygame.quit()
