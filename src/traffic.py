0670d4679a2af443df85f0ad33bfd6b6c0331049
larapoves <98612233+larapoves@users.noreply.github.com>
2024-03-06 10:22:43 +0100

    Update traffic.py

--- src/traffic.py
+++ src/traffic.py
@@ -1,3 +1,3 @@
 # lidar - 3 -> añadir lidar y visualizarlo
 # traffic manager - 2 -> ver que no se chocan, generar trafico en una zona
-# autopilot - 1 -> teleoperador pero con autopilot
+# autopilot - 1 -> teleoperador pero con autopilot, hay diferentes modos
