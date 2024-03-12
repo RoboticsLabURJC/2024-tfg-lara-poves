d719cdc5315998cdf41e0740717a298d5618e3c8
larapoves <poveslara@gmail.com>
2024-03-12 23:05:08 +0100

    lidar

--- src/configcarla.py
+++ src/configcarla.py
@@ -3,80 +3,87 @@ import pygame
 import numpy as np
 import math
 import random
+from typing import Tuple
 
-def get_rect(size, init):
-    sub_screen = pygame.Surface(size)
-    rect = sub_screen.get_rect(topleft=init)
-
-    return rect
 
 class Sensor:
-    def __init__(self, rect, sensor):
-        self.rect = rect
+    def __init__(self, size:Tuple[int, int], init:Tuple[int, int], sensor:carla.Sensor, 
+                 scale:int, color_draw:Tuple[int, int, int], color_screen:Tuple[int, int, int]):
         self.data = None
         self.sensor = sensor
+        self.scale = scale
+        self.color_draw = color_draw
+        self.color_screen = color_screen
 
-        self.sensor.listen(lambda data: self.update_data(data))
+        self.init = (init[0] + size[0] / 2, init[1] + size[1] / 2)
+        self.sub_screen = pygame.Surface(size)
+        self.rect = self.sub_screen.get_rect(topleft=init)
 
-    def update_data(self, data):
+        self.sensor.listen(lambda data: self._update_data(data))
+
+    def _update_data(self, data):
         self.data = data
 
-    def change_rect(self, size, init=(0, 0)):
-        self.rect= get_rect(size, init)
+    def set_rect_screen(self, size=None, init=None):
+        self.sub_screen = pygame.Surface(size)
+        self.rect = self.sub_screen.get_rect(topleft=init)
+
+        if size != None and init != None:
+            self.init = (init[0] + size[0] / 2, init[1] + size[1] / 2)
     
-    def show_image(self, screen):
+    def show_image(self, screen:pygame.Surface):
         if self.data == None or self.rect == None:
             return
         
         if isinstance(self.data, carla.Image):
-            # Convert the image to a numpy array
             array = np.frombuffer(self.data.raw_data, dtype=np.dtype("uint8"))
             array = np.reshape(array, (self.data.height, self.data.width, 4))
 
             # Swap blue and red channels
             array = array[:, :, (2, 1, 0)]
 
-            # Create a Pygame surface 
+            # Reserve mirror effect
             image_surface = pygame.surfarray.make_surface(array)
-
-            # Reverse mirror effect
             flipped_surface = pygame.transform.flip(image_surface, True, False)
 
-            # Resize the image
             screen_surface = pygame.transform.scale(flipped_surface, self.rect.size)
-
             screen.blit(screen_surface, self.rect)
 
+        elif isinstance(self.data, carla.LidarMeasurement):
+            lidar_data = np.copy(np.frombuffer(self.data.raw_data, dtype=np.dtype('f4')))
+            lidar_data = np.reshape(lidar_data, (int(lidar_data.shape[0] / 4), 4))
+            points = lidar_data[:, :-1]
+
+            self.sub_screen.fill(self.color_screen)
+            screen.blit(self.sub_screen, self.rect)
+
+            for x, y, _ in points:
+                center = (int(x * self.scale + self.init[0]), int(y * self.scale + self.init[1]))
+                pygame.draw.circle(screen, self.color_draw, center, 1)
+
 class Vehicle_sensors:
-    def __init__(self, vehicle, world, screen):
+    def __init__(self, vehicle: carla.Vehicle, world: carla.World, screen: pygame.Surface):
         self.vehicle = vehicle
         self.world = world
         self.screen = screen
         self.sensors = []
 
-    def add_sensor(self, sensor, size=None, init=(0, 0), transform=carla.Transform()):
+    def add_sensor(self, sensor:str, size_rect:Tuple[int, int], init:Tuple[int, int]=(0, 0), 
+                   transform:carla.Transform=carla.Transform(), scale_lidar:int=20,
+                   color_screen:Tuple[int, int, int]=(0, 0, 0), 
+                   color_draw:Tuple[int, int, int]=(0, 255, 0)):
         try:
             sensor_bp = self.world.get_blueprint_library().find(sensor)
         except IndexError:
             print("Sensor", sensor, "doesn't exist!")
             return
-        
-        rect = size
-        if size != None: 
-            rect = get_rect(size, init)
                 
         sensor = self.world.spawn_actor(sensor_bp, transform, attach_to=self.vehicle)
-        sensor_class = Sensor(rect=rect, sensor=sensor)
+        sensor_class = Sensor(size=size_rect, init=init, sensor=sensor, scale=scale_lidar, 
+                              color_screen=color_screen, color_draw=color_draw)
         self.sensors.append(sensor_class)
-        
-    def resize_screen(self, width=None, height=None):
-        current_width, current_height = self.screen.get_size()
-        if width == None:
-            width = current_width
-        if height == None:
-            height = current_height
 
-        self.screen = pygame.display.set_mode((width, height))
+        return sensor_class
 
     def update_screen(self):
         for sensor in self.sensors:
@@ -91,7 +98,7 @@ class Vehicle_sensors:
         self.vehicle.destroy()
 
 class Teleoperator:
-    def __init__(self, vehicle, steer=0.3, throttle=0.6, brake=1.0):
+    def __init__(self, vehicle:carla.Vehicle, steer:float=0.3, throttle:float=0.6, brake:float=1.0):
         self.vehicle = vehicle
         self.steer = max(0.0, min(1.0, steer))
         self.throttle = max(0.0, min(1.0, throttle))
@@ -112,17 +119,17 @@ class Teleoperator:
 
         self.vehicle.apply_control(control)
 
-    def set_steer(self, steer):
+    def set_steer(self, steer:float):
         self.steer = max(0.0, min(1.0, steer))
 
-    def set_throttle(self, throttle):
+    def set_throttle(self, throttle:float):
         self.throttle = max(0.0, min(1.0, throttle))
 
-    def set_brake(self, brake):
+    def set_brake(self, brake:float):
         self.brake = max(0.0, min(1.0, brake))
 
-def setup_carla(port=2000, vehicle='vehicle.lincoln.mkz_2020', 
-                name_world='Town01', transform=None):
+def setup_carla(port:int=2000, vehicle:carla.Vehicle='vehicle.lincoln.mkz_2020', 
+                name_world:str='Town01', transform=None):
     # Connect to the server
     client = carla.Client('localhost', port)
     world = client.get_world()
@@ -139,7 +146,8 @@ def setup_carla(port=2000, vehicle='vehicle.lincoln.mkz_2020',
 
     return world, ego_vehicle, client
 
-def center_spectator(world, transform, scale=5.5, height=3.0, pitch=-10.0):
+def center_spectator(world:carla.World, transform:carla.Transform,
+                     scale:float=5.5, height:float=3.0, pitch:float=-10.0):
     yaw = math.radians(transform.rotation.yaw)
     spectator =  world.get_spectator()
 
@@ -149,8 +157,9 @@ def center_spectator(world, transform, scale=5.5, height=3.0, pitch=-10.0):
     transform.rotation.pitch = pitch
 
     spectator.set_transform(transform)
+    return spectator
 
-def setup_pygame(size, name):
+def setup_pygame(size:Tuple[int, int], name:str):
     pygame.init()
     clock = pygame.time.Clock()
 
@@ -159,7 +168,7 @@ def setup_pygame(size, name):
 
     return screen, clock
 
-def add_vehicles(world, number):
+def add_vehicles(world:carla.World, number:int):
     vehicle_bp = world.get_blueprint_library().filter('*vehicle*')
     spawn_points = world.get_map().get_spawn_points()
 
@@ -171,7 +180,8 @@ def add_vehicles(world, number):
 
     return vehicles
 
-def traffic_manager(client, vehicles, port=5000, dist=3.0, speed_lower=10.0):
+def traffic_manager(client:carla.Client, vehicles:carla.Vehicle, port:int=5000, 
+                    dist:float=3.0, speed_lower:float=10.0):
     tm = client.get_trafficmanager(port)
     tm_port = tm.get_port()
 
@@ -179,6 +189,7 @@ def traffic_manager(client, vehicles, port=5000, dist=3.0, speed_lower=10.0):
         v.set_autopilot(True, tm_port)
         tm.auto_lane_change(v, False) 
 
+    speed_lower = max(0.0, min(100.0, speed_lower))
     tm.set_global_distance_to_leading_vehicle(dist)
     tm.global_percentage_speed_difference(speed_lower)
 
