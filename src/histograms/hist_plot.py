ff4f128ac5b023f556b6f08a3f9b2f05cfef7af9
larapoves <poveslara@gmail.com>
2024-04-17 13:35:22 +0200

    lidar terminado y autopiloto corregido

--- src/histograms/hist_plot.py
+++ src/histograms/hist_plot.py
@@ -1,20 +1,37 @@
 import matplotlib.pyplot as plt
+import csv
+import numpy as np
 
-# Datos para el histograma
-data = [5, 15, 20, 25, 30, 35, 35, 40, 45, 50, 55, 60, 65, 70, 75]
+csv_path = '/home/alumnos/lara/2024-tfg-lara-poves/src/histograms/hist_data.csv'
 
-# Tamaño del bin
-bin_size = 10
+with open(csv_path, newline='') as csvfile:
+    reader = csv.reader(csvfile)
 
-# Crear el histograma
-plt.hist(data, bins=range(min(data), max(data) + bin_size, bin_size), edgecolor='black')
+    num_rows = sum(1 for row in reader)
+    csvfile.seek(0) # Go back to the beginning of the file
 
-# Etiquetas y título
-plt.xlabel('Valor')
-plt.ylabel('Frecuencia')
-plt.title('Histograma con size_bin = 10')
+    titles = []
+    data = [[] for _ in range(num_rows)]
+    index = 0
 
-# Mostrar el histograma
+    for row in reader:
+        titles.append(row[0])  
+        data[index].extend(map(float, row[1:]))
+        index += 1
+        
+if num_rows % 2 != 0:
+    num_rows += 1
+
+fig, axs = plt.subplots(num_rows // 2, 2, figsize=(12, 4 * (num_rows // 2)))
+
+for i, (title, d) in enumerate(zip(titles, data)):
+    row = i // 2 
+    col = i % 2  
+    axs[row, col].hist(d, bins=np.linspace(0, 10, num=101), edgecolor='black')
+    axs[row, col].set_title(title)
+    axs[row, col].set_xlabel('Distance (m)')
+    axs[row, col].set_ylabel('Frequency')
+    axs[row, col].set_ylim(0, 26) 
+
+plt.tight_layout()
 plt.show()
-
-
