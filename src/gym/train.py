23a19f491e67cd1a824071635ce5d1ec0fd01a62
larapoves <poveslara@gmail.com>
2024-05-27 19:07:11 +0200

    normalize in train

--- src/gym/train.py
+++ src/gym/train.py
@@ -3,7 +3,8 @@ from stable_baselines3 import DQN, A2C, DDPG, TD3, SAC, PPO
 import argparse
 import os
 import yaml
-from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import VecNormalize
 
 # Visualize train: tensorboard --logdir=log_dir
 
@@ -19,18 +20,23 @@ alg_callable = {
     'PPO': PPO
 }
 
+def check_dir(dir:str, env:str):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+    dir = dir + env.lower()
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+    return dir
+
 def main(args):
     env_dir = args.env.split("-")[0]
     if "MountainCar" in args.env:
         env_dir = "MountainCar"
 
-    log_dir = '/home/alumnos/lara/2024-tfg-lara-poves/src/gym/log/' + env_dir.lower()
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-
-    model_dir = '/home/alumnos/lara/2024-tfg-lara-poves/src/gym/model/' + env_dir.lower()
-    if not os.path.exists(model_dir):
-        os.makedirs(model_dir)
+    log_dir = check_dir('/home/alumnos/lara/2024-tfg-lara-poves/src/gym/log/', env_dir)
+    model_dir = check_dir('/home/alumnos/lara/2024-tfg-lara-poves/src/gym/model/', env_dir)
 
     alg = alg_callable[args.alg]
     log_name = args.alg + '-' + args.env + '-' + str(args.log_interval)
@@ -46,30 +52,45 @@ def main(args):
         print("Algorithm", args.alg, "is not available for environment", args.env)
         return
     
-    # Extract some parameters
+    # Extract compulsory params
     n_timesteps = model_params['n_timesteps']
     model_params.pop('n_timesteps', None)
     policy = model_params['policy']
     model_params.pop('policy', None)
 
-    # Create env
+    # Extrat optional params
     try:
         n_envs = model_params['n_envs']
         model_params.pop('n_envs', None)
-        norm = model_params['normalize']
-        model_params.pop('normalize', None)
-
-        envs = [lambda: gym.make(args.env) for _ in range(n_envs)]
-        env = DummyVecEnv(envs) # Vector of envs
     except KeyError:
-        env = gym.make(args.env, render_mode="human")
+        n_envs = 1
+    try:
+        normalize = model_params['normalize']
+        model_params.pop('normalize', None)
+    except KeyError:
+        normalize = False
+
+    # Create env
+    def make_env(**kwargs) -> gym.Env:
+        spec = gym.spec(args.env)
+        return spec.make(**kwargs)  
+    env = make_vec_env(make_env, n_envs=n_envs, seed=SEED)
+    env = VecNormalize(env, norm_obs=normalize, norm_reward=normalize)
 
     # Create, train and save the model
     model = alg(policy, env, verbose=1, seed=SEED, tensorboard_log=log_dir, **model_params)
     model.set_random_seed(SEED)
     model.learn(total_timesteps=n_timesteps, log_interval=args.log_interval, 
                 tb_log_name=log_name, progress_bar=True)
-    model.save(model_dir + '/' + args.alg + '-' + args.env)
+    cont_save = '/' + args.alg + '-' + args.env
+    model.save(model_dir + cont_save)
+
+    if normalize:
+        # Save the running average, for testing the agent we need that normalization
+        vec_normalize = model.get_vec_normalize_env()
+        assert vec_normalize is not None
+        vec_dir = check_dir('/home/alumnos/lara/2024-tfg-lara-poves/src/gym/vecnormalize/', env_dir)
+        vec_normalize.save(vec_dir + cont_save + ".pkl")
 
 if __name__ == "__main__":
     possible_envs = [
