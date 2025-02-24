deed34fe4da94edf37c90c7f013c13ed8a1d7002
larapoves <poveslara@gmail.com>
2025-02-24 22:46:12 +0100

    corregida percepcion, excepto porfiling

--- src/deepRL/plot/sigmoide.py
+++ src/deepRL/plot/sigmoide.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def sigmoid(x, a=0.1, b=10):
+def sigmoid(x, a=10, b=0.5):
     return 1 / (1 + np.exp(-a * (x - b)))
 
 # Definir el rango de x
-x = np.linspace(0, 135, 400)
-y = sigmoid(x, a=0.1, b=80)
+x = np.linspace(0, 1, 100)
+y = sigmoid(x, a=10, b=0.7)
 
 # Graficar
 plt.plot(x, y, 'o-', label="sigmoid(a=0.1, b=90)")
